7ce980451c97b2d079d7faf702425d69d8ba8cc2
SD2400 <19331074116@163.com>
2026-09-12 17:20:06 +0800

    学生管理系统大致框架

--- main.py
+++ main.py
@@ -1,4 +1,66 @@
-print("hello python")
-print("hello world")
-print("how are you")
-print("学生管理系统")
+print("欢迎进入学生管理系统")
+menu = """
+######学生管理系统#######
+#       1.添加        #
+#       2.查询        #
+#       3.删除        #
+#       4.修改        #
+#       5.统计        #
+#       0.退出        #
+#######################
+"""
+print(menu)
+student = {}     # 建立一个空字典,student = {"id":{"id":"001", "name":"张三","age":20,"score":90 ,"major":"数学与应用数学"},{},...}
+while True:
+    a = int(input("请输入您要使用的功能:"))
+
+    if a == 1:  #添加
+        student_id = input("请输入学生的id:")
+        if student_id in student:
+            print("该学生已存在,请重新输入")
+            continue
+        student_name = input("请输入学生的姓名:")
+        student_age = int(input("请输入学生的年龄:"))
+        student_grades = int(input("请输入学生的成绩:"))
+        student_major = input("请输入学生的专业:")
+        student[student_id] = {"id":student_id,"name":student_name,"age":student_age,"grades":student_grades,"major":student_major}
+        print("该学生已成功添加!")
+    elif a == 2:   #查询
+        student_id = input("请输入您要查询的学生id:")
+        if student_id not in student:
+            print("该学生不存在,请重新输入")
+            continue
+        print(student[student_id])
+
+
+    elif a == 3:    #删除
+        student_id = input("请输入您要删除的学生id:")
+        if student_id not in student:
+            print("该学生不存在,请重新输入")
+            continue
+        del student[student_id]
+
+
+    elif a == 4:    #修改
+        student_id = input("请输入您要修改的学生id:")
+        if student_id not in student:
+            print("该学生不存在,请重新输入")
+            continue
+        student_name = input("请输入要最终的姓名:")
+        student_age = input("请输入最终的年龄:")
+        student_grades = input("请输入最终的成绩:")
+        student_major = input("请输入最终的专业:")
+        student[student_id]={"id":student_id,"name":student_name,"age":student_age,"grades":student_grades,"major":student_major}
+
+    elif a == 5:    #统计
+        for i in student.values():
+            print(i)
+
+    elif a == 0:    #退出
+        print("bye~")
+        break
+
+
+    else:
+     print("您的操作不符合规范,请重新输入~")
+
