a1670a1710ec4e6d665aa81deecafffd90413c29
SD2400 <19331074116@163.com>
2026-09-16 21:09:05 +0800

    完善功能5.

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ menu = """
 #       0.退出        #
 #######################
 """
-print(menu)
+print(menu)      #输出菜单
 student = {}     # 建立一个空字典,student = {"id":{"id":"001", "name":"张三","age":20,"score":90 ,"major":"数学与应用数学"},{},...}
-while True:
+while True:      #while循环
     a = int(input("请输入您要使用的功能:"))
 
     if a == 1:  #添加
         student_id = input("请输入学生的id:")
         if student_id in student:
             print("该学生已存在,请重新输入")
-            continue
+            continue #退出当前循环,开始下一次循环
         student_name = input("请输入学生的姓名:")
         student_age = int(input("请输入学生的年龄:"))
         student_grades = int(input("请输入学生的成绩:"))
@@ -53,12 +53,23 @@ while True:
         student[student_id]={"id":student_id,"name":student_name,"age":student_age,"grades":student_grades,"major":student_major}
 
     elif a == 5:    #统计
-        for i in student.values():
-            print(i)
+            abc=[]                  #定义一个空列表
+            pass_grades = 0         # 初始及格人数
+            for i in student.values():    #for循环,对字典student中的值进行遍历
+                abc.append(i["grades"])  #将grades中的值添加进abc空列表中
+                if i["grades"] >= 60:  #if条件判断
+                    pass_grades += 1
+            max_grades = max(abc)  #求最大值
+            min_grades = min(abc)  #求最小值
+            avg_grades = sum(abc)/len(abc)  #求平均值
+            print(f"分数最高为:{max_grades},分数最低为:{min_grades},平均分为:{avg_grades},及格人数为:{pass_grades}")
+
+
+
 
     elif a == 0:    #退出
         print("bye~")
-        break
+        break           #退出循环
 
 
     else:
